476300922774351e6a1e66ac849b6cf68379e282
Lolors <rolilin8@gmail.com>
2026-07-05 22:34:26 +0900

    Hide last sale importer on outbound page

--- nohtus/pages/outbound_business.py
+++ nohtus/pages/outbound_business.py
@@ -1,16 +1,13 @@
-import streamlit as st
-
 import nohtus.pages.outbound as outbound_page
 
 
-def _render_last_sale_notice():
-    with st.expander("최근거래일 갱신", expanded=False):
-        st.info("최근거래일 갱신용 매출 파일 업로드는 [기초 > 거래처 관리] 메뉴에서 진행하세요.")
+def _hide_last_sale_importer():
+    return None
 
 
 def page_outbound():
     original_renderer = outbound_page._render_last_sale_importer
-    outbound_page._render_last_sale_importer = _render_last_sale_notice
+    outbound_page._render_last_sale_importer = _hide_last_sale_importer
     try:
         return outbound_page.page_outbound()
     finally:
