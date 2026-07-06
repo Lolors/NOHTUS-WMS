2a2ac7465460906448c528eedc401ed43adec234
Lolors <rolilin8@gmail.com>
2026-07-06 23:01:03 +0900

    Add GM Medic runtime special location

--- nohtus/config_runtime.py
+++ nohtus/config_runtime.py
@@ -2,4 +2,4 @@ APP_TITLE = "NOHTUS WMS"
 VERSION = "v4.9 RC3.3 UI Stable"
 COMPANIES = ["노투스팜", "노투스", "NOH", "비자료"]
 INBOUND_COMPANIES = COMPANIES + ["등록대기"]
-SPECIAL_LOCATIONS = ["홍보물랙", "회색 카트", "오른쪽 창고", "사무실(4층)"]
+SPECIAL_LOCATIONS = ["홍보물랙", "회색 카트", "오른쪽 창고", "사무실(4층)", "지엠메딕"]
