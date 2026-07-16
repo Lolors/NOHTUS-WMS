cccaa9a5b57886c4f169f7476776d7687ce35133
Lolors <rolilin8@gmail.com>
2026-07-16 16:04:08 +0900

    Use current DB expiry in outbound warning

--- nohtus/pages/__init__.py
+++ nohtus/pages/__init__.py
@@ -5,3 +5,4 @@ Legacy Streamlit page functions will be moved here one by one.
 
 # 출고 페이지가 로드되기 전에 제조번호별 출고 경고를 등록한다.
 from nohtus.pages import outbound_lot_warning as _outbound_lot_warning  # noqa: F401,E402
+from nohtus.pages import outbound_lot_warning_db_refresh as _outbound_lot_warning_db_refresh  # noqa: F401,E402
